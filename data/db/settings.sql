d233e47e7f7e090fcfa4d0ed19e1621646c50c5f
tanakei <100189072+GDur4n66@users.noreply.github.com>
2023-05-26 22:36:53 +0900

    Update settings.sql

--- data/db/settings.sql
+++ data/db/settings.sql
@@ -7,7 +7,7 @@ DELETE FROM settings;
 -- https://github.com/boltcard/boltcard/blob/main/docs/SETTINGS.md
 
 INSERT INTO settings (name, value) VALUES ('LOG_LEVEL', 'DEBUG');
-INSERT INTO settings (name, value) VALUES ('AES_DECRYPT_KEY', 'ランダム値'); -- set this. $ hexdump -vn16 -e'4/4 "%08x" 1 "\n"' /dev/random
+INSERT INTO settings (name, value) VALUES ('AES_DECRYPT_KEY', 'ランダム値'); -- set this. この値がk1になる。生成例) hexdump -vn16 -e'4/4 "%08x" 1 "\n"' /dev/random
 INSERT INTO settings (name, value) VALUES ('HOST_DOMAIN', '自分のドメイン名'); -- set this.
 INSERT INTO settings (name, value) VALUES ('MIN_WITHDRAW_SATS', '1');
 INSERT INTO settings (name, value) VALUES ('MAX_WITHDRAW_SATS', '1000000');
@@ -26,5 +26,5 @@ INSERT INTO settings (name, value) VALUES ('AWS_SES_SECRET', '');
 INSERT INTO settings (name, value) VALUES ('AWS_SES_EMAIL_FROM', '');
 INSERT INTO settings (name, value) VALUES ('EMAIL_MAX_TXS', '');
 INSERT INTO settings (name, value) VALUES ('FUNCTION_LNDHUB', 'ENABLE');
-INSERT INTO settings (name, value) VALUES ('LNDHUB_URL', 'http://10.21.100.103:9002'); -- see docker-compose.yml
+INSERT INTO settings (name, value) VALUES ('LNDHUB_URL', 'http://boltcard_lndhub:9002'); -- see docker-compose.yml
 INSERT INTO settings (name, value) VALUES ('FUNCTION_INTERNAL_API', 'ENABLE');
